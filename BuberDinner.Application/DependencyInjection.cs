dd7ac793f610e4bae8c5f4c5c2c7d15f955068f3
Gustavo Hurtado Cristia <ghccristia@gmail.com>
2022-12-24 12:56:15 -0500

    Implementing a RegisterCommand validation pipeline behavior

--- BuberDinner.Application/DependencyInjection.cs
+++ BuberDinner.Application/DependencyInjection.cs
@@ -1,4 +1,10 @@
-﻿using MediatR;
+﻿using System.Reflection;
+using BuberDinner.Application.Authentication.Commands.Register;
+using BuberDinner.Application.Authentication.Common;
+using BuberDinner.Application.Common.Behaviors;
+using ErrorOr;
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BuberDinner.Application;
@@ -8,6 +14,12 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(typeof(DependencyInjection).Assembly);
+        services.AddScoped<
+            IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>>,
+            ValidationRegisterCommandBehavior
+        >();
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
         return services;
     }
